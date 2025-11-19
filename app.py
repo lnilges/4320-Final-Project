772fdb52643233ea3f8ba678089182916aca7142
Lani <LMNWKX@umsystem.edu>
2025-11-19 12:09:30 -0600

    Added other html templates & connection to database

--- app.py
+++ app.py
@@ -25,15 +25,65 @@
 
 import flask 
 from flask import Flask, render_template, request, url_for, flash, redirect 
+import sqlite3
  
 
 app = flask.Flask(__name__) 
-app.config["DEBUF"] = True 
-app.config['SECRET_KEY'] = 'your secret key' 
+app.config["DEBUG"] = True 
+app.config['SECRET_KEY'] = 'your secret key'
+
+# Function to open a connection to the database.db file
+def get_db_connection():
+    # create connection to the database
+    conn = sqlite3.connect('reservations.db')
+    
+    # allows us to have name-based access to columns
+    # the database connection will return rows we can access like regular Python dictionaries
+    conn.row_factory = sqlite3.Row
+
+    #return the connection object
+    return conn
+
+#function to get admin login information from database
+
+
+#function to get reservation information from database
+
+#function to generate cost matrix for flights
+#input: none
+#output: returns a 12 x 4 matrix of prices
+def get_cost_matrix():
+    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+    return cost_matrix
+
+
+#route to get to admin page 
+#will display seating chart, total sales, and reservation list with delete button
+@app.route('/admin', methods=('GET', 'POST'))
+def admin():
+   return render_template('admin.html')
+
+#route to get to reservations page
+#will have a form where users can enter first name, last name, seat row, and seat column
+#will then display a reservation code one reservation is made
+@app.route('/reservations', methods=('GET', 'POST'))
+def reservations():
+    return render_template('reservations.html')
 
  
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    choice = request.form.get('user_choice')
 
- 
-
+    if not choice:
+        flash("ERROR: Must choose an option.")
+        return(redirect(url_for('index')))
+    
+    if choice == "admin":
+        return render_template('admin.html')
+    if choice == "reservation":
+        return render_template('reservation.html')
+    else:
+        return redirect((url_for('index')))
 
 app.run() 
